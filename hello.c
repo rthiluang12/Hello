2a320c5d14ae03939ae88850a5c38355f2b0e82b
rthiluang12 <rthiluang2@huskers.unl.edu>
2019-08-29 18:49:05 -0500

    Update hello.c
    Updated Hello to say my name and current major.

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
- * Author: Chris Bourke
- * Date: 2016/11/02
+ * Author: Roam Thiluang
+ * Date: 2019/08/29
  *
  * A simple hello world program in C
  *
@@ -10,7 +10,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello World!\n");
+  printf("Hello Roam\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
